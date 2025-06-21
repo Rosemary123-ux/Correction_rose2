a5a4925d800a072246e0c71469d469b2b9a48b34
rosemary <rosemarychidimma123@gmail.com>
2025-06-21 15:23:03 +0100

    Add files via upload

---
+++ table2.js
@@ -0,0 +1,14 @@
+function multiplicationTable(num) {
+  
+  for (let i = 1; i <= 12; i++) {
+
+    console.log(`${num} x ${i} = ${num * i}`);
+  }
+}
+
+multiplicationTable(6);
+
+console.log(num);
+
+
+
